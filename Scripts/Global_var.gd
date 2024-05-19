b8d19139cd9b0be5f05657cb4d93a4a41ef95e3e
KeyCodeBoard <keycodeboard@gmail.com>
2024-05-19 04:26:31 -0600

    Enemigo 1.0

--- Scripts/Global_var.gd
+++ Scripts/Global_var.gd
@@ -1,9 +1,14 @@
 extends Node
 
 var chatarra_count
-var vida_jugador = 10
 var pausa : bool = false
 var char_state : String
+
+#Salud
+var vida_jugador = 10
+var vida_enemigo = 5
+
+
 
 func _ready():
 	chatarra_count = 10
